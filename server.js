2a0802fcc97106d8efa17049f15d3f5961d4766d
Dinospider <30833694+Dinospider@users.noreply.github.com>
2017-08-15 12:00:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/gone', function (req, res) {
-  res.send('gone');
+  res.send('THIS IS RES');
 });
 
 // Do not change port, otherwise your app won't run on IMAD servers
